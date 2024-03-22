1d2ea37975c97f1ad350abb0c9d11bb7bfab6ada
v-nebe <katya.shauneva@gmail.com>
2024-03-22 11:06:21 +0300

    Diploma project

--- src/main/java/com/shavneva/billingserver/config/SecurityConfig.java
+++ src/main/java/com/shavneva/billingserver/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -30,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf()
                 .disable()
-                .httpBasic(withDefaults())
+                .httpBasic(basic -> basic.securityContextRepository(new HttpSessionSecurityContextRepository()))
                 .authorizeRequests()
                 .requestMatchers("/api/**")
                 .fullyAuthenticated()
