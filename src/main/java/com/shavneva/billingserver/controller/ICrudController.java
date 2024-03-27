d99e4341e451b285a4d59c3e06b392523349a9ff
v-nebe <katya.shauneva@gmail.com>
2024-03-27 17:18:35 +0300

    Diploma project

--- src/main/java/com/shavneva/billingserver/controller/ICrudController.java
+++ src/main/java/com/shavneva/billingserver/controller/ICrudController.java
@@ -25,13 +25,11 @@ public interface ICrudController<DTO> {
 
     //update
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
-    @PostFilter("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     @PutMapping("/update")
     DTO update(@Valid @RequestBody DTO newDTO);
 
     //delete
     @PreAuthorize("principal.userId == #id or hasRole('ROLE_ADMIN')")
-    @PostFilter("principal.userId == #id or hasRole('ROLE_ADMIN')")
     @DeleteMapping("/delete/{id}")
     void delete(@Valid @PathVariable int id);
 }
