3bc01faa0794a133f355d117ddc9c3a975eead87
v-nebe <katya.shauneva@gmail.com>
2024-04-19 13:40:34 +0300

    Diploma project

---
+++ src/main/java/com/shavneva/billingserver/service/impl/EmailNotificationService.java
@@ -0,0 +1,13 @@
+package com.shavneva.billingserver.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailNotificationService {
+
+    public void sendNotification(String userEmail, String subject, String message) {
+
+    }
+
+}
---
+++ src/main/java/com/shavneva/billingserver/service/impl/SmsNotificationService.java
@@ -0,0 +1,11 @@
+package com.shavneva.billingserver.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsNotificationService {
+
+    public void sendNotification(String phoneNumber, String message) {
+
+    }
+}
