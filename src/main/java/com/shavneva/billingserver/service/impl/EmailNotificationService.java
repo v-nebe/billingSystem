211ccaa15e1391a9db351bbb59a9e430304b84e5
v-nebe <katya.shauneva@gmail.com>
2024-04-22 13:46:43 +0300

    Diploma project

--- src/main/java/com/shavneva/billingserver/service/impl/EmailNotificationService.java
+++ src/main/java/com/shavneva/billingserver/service/impl/EmailNotificationService.java
@@ -1,13 +1,25 @@
 package com.shavneva.billingserver.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailNotificationService {
 
-    public void sendNotification(String userEmail, String subject, String message) {
+    private final JavaMailSender emailSender;
 
+    @Autowired
+    public EmailNotificationService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
     }
 
+    public void sendNotification(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
 }
